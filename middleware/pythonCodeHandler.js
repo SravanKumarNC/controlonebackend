510b8e5c402fadd881f4ce1e32053256146251c8
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-22 14:09:30 +0530

    Add files via upload

---
+++ middleware/pythonCodeHandler.js
@@ -0,0 +1,38 @@
+const { spawn } = require('child_process');
+
+let pythonProcess = null;
+
+function startPythonScript(callback) {
+    if (!pythonProcess) {
+        pythonProcess = spawn('python', ['E:\\python\\updateData.py']);
+
+        pythonProcess.stdout.on('data', (data) => {
+            console.log(`Python script output: ${data}`);
+        });
+
+        pythonProcess.stderr.on('data', (data) => {
+            console.error(`Python script error: ${data}`);
+        });
+
+        pythonProcess.on('close', (code) => {
+            console.log(`Python script process exited with code ${code}`);
+            pythonProcess = null; // Reset the process variable after completion
+        });
+
+        callback('Python script started');
+    } else {
+        callback('Python script is already running');
+    }
+}
+
+function stopPythonScript(callback) {
+    if (pythonProcess) {
+        pythonProcess.kill(); // Terminate the Python process
+        pythonProcess = null; // Reset the process variable
+        callback('Python script stopped');
+    } else {
+        callback('No running Python script to stop');
+    }
+}
+
+module.exports = { startPythonScript, stopPythonScript };
