51d502068bff4711d59ca8dbd663d4dd758b7abd
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-22 14:14:15 +0530

    Update forklift.js

--- routes/forklift.js
+++ routes/forklift.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const forkliftController = require('../controller/forkliftControl');
+const  { authenticateToken }  = require('../middleware/authMiddleware');
+
 
 const router = express.Router();
+
+router.use('/api/forklifts', authenticateToken); 
 
 router.post('/api/forklifts', forkliftController.createForklift);
 
@@ -12,5 +16,9 @@ router.get('/api/forklifts/:id', forkliftController.getForkliftById);
 router.put('/api/forklifts/:id', forkliftController.updateForklift);
 
 router.delete('/api/forklifts/:id', forkliftController.deleteForklift);
+
+router.get('/protected_route', authenticateToken, (req, res) => {
+    res.json({ message: 'Access granted', user: req.user });
+});
 
 module.exports = router;
