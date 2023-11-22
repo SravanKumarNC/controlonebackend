14b072d384d8ac12926aac15a8325b5c8ae07513
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-22 15:53:03 +0530

    Update pythonCodeHandler.js

--- middleware/pythonCodeHandler.js
+++ middleware/pythonCodeHandler.js
@@ -1,7 +1,7 @@
 const { spawn } = require('child_process');
 
 
-const pythonScriptPath = '../backend/updateData.py'; // Replace with your Python script path
+const pythonScriptPath = './updateData.py'; // Replace with your Python script path
 let pythonProcess = null;
 
 function startPythonScript(callback) {
