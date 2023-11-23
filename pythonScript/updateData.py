69011615d62ed5aa9bb35d74a085be2f8294a0c6
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-23 12:40:21 +0530

    Update updateData.py

--- pythonScript/updateData.py
+++ pythonScript/updateData.py
@@ -1,21 +1,40 @@
-import matplotlib.pyplot as plt
-import numpy as np
+const { spawn } = require('child_process');
 
-# Generate some data
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
 
-# Create a plot
-plt.figure(figsize=(8, 6))
-plt.plot(x, y, label='sin(x)')
-plt.title('Sine Wave')
-plt.xlabel('x-axis')
-plt.ylabel('y-axis')
-plt.legend()
-plt.grid(True)
+const pythonScriptPath = './updateData.py'; // Replace with your Python script path
+let pythonProcess = null;
 
-# Save the plot as an image (optional)
-plt.savefig('sine_wave_plot.png')
+function startPythonScript(callback) {
+    if (!pythonProcess) {
+        pythonProcess = spawn('python', [pythonScriptPath]);
 
-# Show the plot (optional, will display a window with the plot)
-plt.show()
+        pythonProcess.stdout.on('data', (data) => {
+            console.log(`Python script output: ${data}`);
+        });
+
+        pythonProcess.stderr.on('data', (data) => {
+            console.error(`Python script error: ${data}`);
+        });
+
+        pythonProcess.on('close', (code) => {
+            console.log(`Python script process exited with code ${code}`);
+            pythonProcess = null; // Reset the process variable after completion
+        });
+
+        callback('Python script started');
+    } else {
+        callback('Python script is already running');
+    }
+}
+
+function stopPythonScript(callback) {
+    if (pythonProcess) {
+        pythonProcess.kill(); // Terminate the Python process
+        pythonProcess = null; // Reset the process variable
+        callback('Python script stopped');
+    } else {
+        callback('No running Python script to stop');
+    }
+}
+
+module.exports = { startPythonScript, stopPythonScript };
