1f304ba176e52fe8f6c6d157897215b7327879fc
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-11-29 20:23:52 +0530

    Update driver.js

--- routes/driver.js
+++ routes/driver.js
@@ -14,10 +14,11 @@ const  { authenticateToken }  = require('../middleware/authMiddleware');
 // router.use('/api/driver', authenticateToken); 
 router.get('/api/driver', driverController.getAllDriver);
 router.post('/api/driver', driverController.createDriver);
+router.put('/api/driver/makefalse/:username',driverController.makeFalse)
+router.put('/api/driver/maketrue/:username',driverController.makeTrue)
 router.get('/api/driver/:username', driverController.getDriverByUsername);
 router.put('/api/driver/:username', driverController.updateDriver);
 router.delete('/api/driver/:username', driverController.deleteDriver);
-
 
 // router.get('/start_code', (req, res) => {
 //     const urlToOpen = 'https://webrtc.github.io/test-pages/src/audio-and-video/'; // Replace with the URL you want to open
