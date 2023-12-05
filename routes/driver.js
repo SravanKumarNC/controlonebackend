60ef1b6265efe78cc1bbafbf88e85b65bd6cc5fc
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-12-05 12:59:12 +0530

    Update driver.js

--- routes/driver.js
+++ routes/driver.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const path = require('path');
-// const puppeteer = require('puppeteer');
-// const os = require('os');
-// const cheerio = require('cheerio');
 const driverController = require('../controller/driverControl')
 const  { authenticateToken }  = require('../middleware/authMiddleware');
 
@@ -19,20 +15,6 @@ router.put('/api/driver/maketrue/:username',driverController.makeTrue)
 router.get('/api/driver/:username', driverController.getDriverByUsername);
 router.put('/api/driver/:username', driverController.updateDriver);
 router.delete('/api/driver/:username', driverController.deleteDriver);
-
-// router.get('/start_code', (req, res) => {
-//     const urlToOpen = 'https://webrtc.github.io/test-pages/src/audio-and-video/'; // Replace with the URL you want to open
-//     res.json({ url: urlToOpen });
-//     // startPythonScript((result) => {
-//     //     res.send(result);
-//     // });
-// });
-
-// router.post('/stop_code', (req, res) => {
-//     stopPythonScript((result) => {
-//         res.send(result);
-//     });
-// });
 
 
 router.get('/protected_route', authenticateToken, (req, res) => {
