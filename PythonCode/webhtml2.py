2916db3beed911330491c11df8f58ac3417d1b10
Sudev K U <64310688+sudu-sudev@users.noreply.github.com>
2023-12-05 13:27:42 +0530

    Delete PythonCode directory

--- PythonCode/webhtml2.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-# Search for Chrome profile directory
-chrome_profile_directory = Path("/home/render/.config/google-chrome/Profile 1")
-
-if chrome_profile_directory.exists():
-    print("Chrome profile directory found:", chrome_profile_directory)
-else:
-    print("Chrome profile directory not found.")
